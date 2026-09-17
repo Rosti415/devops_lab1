35c938f255c77c41a0bf5d29e7e0876ef47967fb
Rostyslav Yakubovskiy <116059105+Rosti415@users.noreply.github.com>
2026-09-17 21:11:12 +0300

    додані коментарі до фцнкцій

--- lib.py
+++ lib.py
@@ -1,9 +1,10 @@
+# функція для перевірки надійності пароля
 def check_password(password):
     if len(password) < 8:
         return "Слабкий пароль: менше 8 символів"
     else:
         return "Пароль задовільний"
-
+# функція для створення акаунта
 def account(login,reliability):
     message = "Акаунт: " + login + " -> " + reliability
     return message
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import lib
+import lib # імпортуємо бібліотеку lib.py
 
 my_login = "Rostyslav"
 my_password = "12345678"
 
+# перевіряємо надійність пароля та створення акаунта
 status = lib.check_password(my_password)
 result = lib.account(my_login, status)
 
